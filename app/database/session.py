754d880f5af7fed4638e894eefa861c9086364eb
Aleksandr Bardusov <96628661+tndrcloud@users.noreply.github.com>
2023-07-21 10:33:53 +0300

    Create session.py

---
+++ app/database/session.py
@@ -0,0 +1,17 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
+from sqlalchemy.orm import sessionmaker
+from settings import settings 
+
+
+Base: DeclarativeMeta = declarative_base()
+
+
+engine = create_async_engine(settings.DATABASE_URL)
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
