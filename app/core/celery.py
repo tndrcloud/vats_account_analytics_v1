d5cc502215e3a0b756ba8d515ad85f6b1d7ef8a0
Aleksandr <alexbardysov96@gmail.com>
2023-07-28 11:43:37 +0000

    add redis cache + celery

---
+++ app/core/celery.py
@@ -0,0 +1,11 @@
+from celery import Celery
+from settings import settings
+
+
+celery = Celery("tasks", broker=f"redis://localhost:{settings.REDIS_PORT}")
+
+
+"""
+RUN CELERY APP: celery -A core.celery:celery worker --loglevel=INFO
+RUN FLOWER: celery -A core.celery flower (to localhost:5555)
+"""
